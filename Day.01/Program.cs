a948723a0f6cca91925d3b9de422635c4403442c
Alex Holsopple <alex@Alexs-MBP.lan>
2021-12-01 18:11:58 -0600

    init

---
+++ Day.01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day01;
+
+var samples = new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+
+var depthReader = new DepthReader(DepthSamples.AllSamples);
+
+Console.WriteLine($"Total number of samples: {depthReader.TotalDepthSamples}.");
+Console.WriteLine($"Total number of depth increases: {depthReader.DepthIncreases}.");
