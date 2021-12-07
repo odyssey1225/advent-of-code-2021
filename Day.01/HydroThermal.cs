911f47bde8eff508ac683240e47313a2bd9adca0
Alex Holsopple <abholsopple@gmail.com>
2021-12-06 19:29:32 -0600

    Plot diagonal lines.

--- Day.01/HydroThermal.cs
+++ Day.01/HydroThermal.cs
@@ -6,18 +6,25 @@ namespace Day01
     {
         public static int GetDangerousPoints(IEnumerable<Tuple<Point, Point>> points)
         {
-            var filteredPoints = points
-                .Where(w => HyrdroThermalExtensions.IsHorizontalLine(w) || HyrdroThermalExtensions.IsVerticalLine(w))
-                .ToList();
+            var grid = BuildGrid(points);
 
-            var grid = BuildGrid(filteredPoints);
-
-            foreach (var line in filteredPoints)
+            foreach (var line in points)
             {
-                grid.Plot(line);
+                switch (HyrdroThermalExtensions.LineType(line))
+                {
+                    case LineType.Horizontal:
+                        grid.PlotHorizontalLine(line);
+                        break;
+                    case LineType.Vertical:
+                        grid.PlotVerticalLine(line);
+                        break;
+                    case LineType.Diagonal:
+                        grid.PlotDiagonalLine(line);
+                        break;
+                }
             }
 
-            return grid.DangerousPointCount();
+            return grid.DangerousPoints();
         }
 
         private static int[,] BuildGrid(IEnumerable<Tuple<Point,Point>> points)
@@ -39,7 +46,7 @@ namespace Day01
 
     internal static class HyrdroThermalExtensions
     {
-        internal static int DangerousPointCount(this int[,] grid, int dangerThreshold = 1)
+        internal static int DangerousPoints(this int[,] grid, int dangerThreshold = 1)
         {
             var dangerousPoints = 0;
 
@@ -54,53 +61,79 @@ namespace Day01
             return dangerousPoints;
         }
 
-        internal static int GetLineLength(int start, int end)
+        internal static LineType LineType(Tuple<Point,Point> line)
+        {
+            return IsHorizontalLine(line)
+                ? Day01.LineType.Horizontal
+                : IsVerticalLine(line)
+                    ? Day01.LineType.Vertical 
+                    : Day01.LineType.Diagonal;
+        }
+
+        internal static void PlotHorizontalLine(this int[,] grid, Tuple<Point, Point> line)
+        {
+            var startingPoint = GetStartingPoint(line.Item1.X, line.Item2.X);
+
+            for (int i = 0; i <= NumberOfPoints(line.Item1.X, line.Item2.X); i++)
+            {
+                grid[startingPoint + i, line.Item1.Y]++;
+            }
+        }
+
+        internal static void PlotVerticalLine(this int[,] grid, Tuple<Point, Point> line)
+        {
+            var startingPoint = GetStartingPoint(line.Item1.Y, line.Item2.Y);
+
+            for (int i = 0; i <= NumberOfPoints(line.Item1.Y, line.Item2.Y); i++)
+            {
+                grid[line.Item1.X, startingPoint + i]++;
+            }
+        }
+
+        internal static void PlotDiagonalLine(this int[,] grid, Tuple<Point, Point> line)
+        {
+            var horizontalIsIncreasing = line.Item1.X < line.Item2.X;
+            var verticalIsIncreasing = line.Item1.Y < line.Item2.Y;
+
+            for (int i = 0; i <= NumberOfPoints(line.Item1.X, line.Item2.X); i++)
+            {
+                var x = horizontalIsIncreasing
+                    ? line.Item1.X + i
+                    : line.Item1.X - i;
+
+                var y = verticalIsIncreasing
+                    ? line.Item1.Y + i
+                    : line.Item1.Y - i;
+
+                grid[x, y]++;
+            }
+        }
+
+        private static int NumberOfPoints(int start, int end)
         {
             return start > end ? start - end : end - start;
         }
 
-        internal static int GetStartingPoint(int start, int end)
+        private static int GetStartingPoint(int start, int end)
         {
             return start < end ? start : end;
         }
 
-        internal static bool IsHorizontalLine(Tuple<Point, Point> line)
+        private static bool IsHorizontalLine(Tuple<Point, Point> line)
         {
             return line.Item1.Y == line.Item2.Y;
         }
 
-        internal static bool IsVerticalLine(Tuple<Point, Point> line)
+        private static bool IsVerticalLine(Tuple<Point, Point> line)
         {
             return line.Item1.X == line.Item2.X;
         }
+    }
 
-        internal static void Plot(this int[,] grid, Tuple<Point, Point> line)
-        {
-            var isHorizontalLine = IsHorizontalLine(line);
-
-            var constant = isHorizontalLine
-                ? line.Item1.Y
-                : line.Item1.X;
-
-            var lineLength = isHorizontalLine
-                ? GetLineLength(line.Item1.X, line.Item2.X)
-                : GetLineLength(line.Item1.Y, line.Item2.Y);
-
-            var startingPoint = isHorizontalLine
-                ? GetStartingPoint(line.Item1.X, line.Item2.X)
-                : GetStartingPoint(line.Item1.Y, line.Item2.Y);
-
-            for (int i = 0; i <= lineLength; i++)
-            {
-                if (isHorizontalLine)
-                {
-                    grid[startingPoint + i, constant]++;
-                }
-                else
-                {
-                    grid[constant, startingPoint + i]++;
-                }
-            }
-        }
+    public enum LineType
+    {
+        Horizontal = 0,
+        Vertical,
+        Diagonal
     }
 }
