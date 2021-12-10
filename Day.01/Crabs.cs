aae87736a0651eddff69c1a688c029fdd830f22c
Alex Holsopple <abholsopple@gmail.com>
2021-12-10 16:30:47 -0600

    Calculate cheapest fuel cost for crab submarines.

---
+++ Day.01/Crabs.cs
@@ -0,0 +1,19 @@
+﻿namespace Day01
+{
+    internal class Crabs
+    {
+        public static int FindCheapestPosition(List<int> crabPositions)
+        {
+            var mostCommon = crabPositions.GroupBy(g => g).OrderBy(o => o.Count()).Select(s => s.Key);
+            var cost = 0;
+
+            foreach(var position in mostCommon)
+            {
+                var currentCost = crabPositions.Select(s => Math.Abs(s - position)).Sum();
+                if (currentCost < cost || cost == 0) cost = currentCost;
+            }
+
+            return cost;
+        }
+    }
+}
