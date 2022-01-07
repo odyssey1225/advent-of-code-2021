296e753d64f4862fef2375299f8fed5de1475129
Alex Holsopple <abholsopple@gmail.com>
2022-01-06 23:47:37 -0600

    Find invalid characters in navigation data, get the invalid character score.

---
+++ Day.01/Day10.cs
@@ -0,0 +1,61 @@
+﻿namespace Day01;
+
+public static class Day10
+{
+    public static long GetCorruptionPoints(IEnumerable<string> input)
+    {
+        var score = 0L;
+
+        foreach (var line in input)
+        {
+            var openingChars = new Stack<char>();
+            
+            foreach(var character in line.ToCharArray())
+            {
+                
+                if (IsOpeningChar(character))
+                {
+                    openingChars.Push(character);
+                    continue;
+                }
+
+                if (IsMatchingPair(openingChars.Pop(), character))
+                {
+                    continue;
+                }
+                
+                score += GetScore(character);
+                break;
+            }
+        }
+        
+        return score;
+    }
+
+    private static readonly char[] OpeningChars = { '(', '[', '{', '<' };
+    
+    private static bool IsOpeningChar(char c)
+    {
+        return OpeningChars.Contains(c);
+    }
+
+    private static bool IsMatchingPair(char a, char b)
+    {
+        return a == '(' && b == ')'
+            || a == '[' && b == ']'
+            || a == '{' && b == '}'
+            || a == '<' && b == '>';
+    }
+
+    private static int GetScore(char c)
+    {
+        return c switch
+        {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => throw new Exception($"Invalid character \"{c}\"")
+        };
+    }
+}
