75beaae9cf4ef891e6c97432fbcea30d6ce0df08
Alex Holsopple <abholsopple@gmail.com>
2022-02-02 03:39:27 -0600

    Moved the bit vector extensions into a separate file. Parse the bits from the input, read packet version and type, decode number literals.

--- Day.01/Diagnostics.cs
+++ Day.01/Diagnostics.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Specialized;
+using Utilities;
 
 namespace Day01
 {
@@ -10,23 +11,23 @@ namespace Day01
 
         public Diagnostics(IEnumerable<string> bitVectorStrings)
         {
-            _bitVectors = DiagnosticsExtensions.ToBitVectorArray(bitVectorStrings);
+            _bitVectors = BitVectorExtensions.ToBitVectorArray(bitVectorStrings);
             _maxNumBits = bitVectorStrings.Select(s => s.Length).Max();
-            _section = DiagnosticsExtensions.GetSection(_maxNumBits);
+            _section = BitVectorExtensions.GetSection(_maxNumBits);
         }
 
         public int GetPowerConsumption()
         {
             var resultBitVector = new BitVector32();
 
-            for (int i = 0; i < _maxNumBits; i++)
+            for (var i = 0; i < _maxNumBits; i++)
             {
-                resultBitVector[DiagnosticsExtensions.GetMask(_maxNumBits - i)] = 
+                resultBitVector[BitVectorExtensions.GetMask(_maxNumBits - i)] = 
                     _bitVectors.MoreBitsAreSet(_maxNumBits - i);
             }
 
             return resultBitVector[_section]
-                * DiagnosticsExtensions.FlipBitVector(resultBitVector, _maxNumBits)[_section];
+                * BitVectorExtensions.FlipBitVector(resultBitVector, _maxNumBits)[_section];
         }
 
         public int GetLifeSupportRating()
@@ -62,67 +63,6 @@ namespace Day01
             return subset.Count == 1
                 ? subset.First()[_section]
                 : GetCO2Rating(subset, ++index);
-        }
-    }
-
-    internal static class DiagnosticsExtensions
-    {
-        internal static bool MoreBitsAreSet(this IReadOnlyCollection<BitVector32> bitVectors, int position)
-        {
-            return bitVectors.GetVectorsByPositionAndState(position, true).Count > bitVectors.Count / 2;
-        }
-
-        internal static IReadOnlyCollection<BitVector32> GetVectorsByPositionAndState(this IReadOnlyCollection<BitVector32> bitVectors, int position, bool isSet)
-        {
-            return bitVectors.Where(w => isSet ? BitIsSet(w, position) : !BitIsSet(w, position)).ToList().AsReadOnly();
-        }
-
-        internal static bool BitIsSet(BitVector32 bitVector, int position)
-        {
-            return bitVector[GetMask(position)];
-        }
-
-        internal static int GetMask(int position)
-        {
-            return position == 1
-                ? BitVector32.CreateMask()
-                : BitVector32.CreateMask((short)Math.Pow(2, position - 2));
-        }
-
-        internal static BitVector32.Section GetSection(int numBits)
-        {
-            return BitVector32.CreateSection((short)(Math.Pow(2, numBits - 1) + 1));
-        }
-
-        internal static BitVector32 FlipBitVector(BitVector32 original, int numBits)
-        {
-            var flippedBitVector = new BitVector32();
-
-            for (int i = 0; i < numBits; i++)
-            {
-                var mask = GetMask(numBits - i);
-                flippedBitVector[mask] = !original[mask];
-            }
-
-            return flippedBitVector;
-        }
-
-        public static IReadOnlyCollection<BitVector32> ToBitVectorArray(IEnumerable<string> data)
-        {
-            return data.Select(s => ToBitVector(s)).ToList().AsReadOnly();
-        }
-
-        private static BitVector32 ToBitVector(string input)
-        {
-            var bitVector = new BitVector32(0);
-            var characters = input.ToCharArray();
-
-            for (int i = 0; i < characters.Length; i++)
-            {
-                bitVector[GetMask(characters.Length - i)] = characters[i] == '1';
-            }
-
-            return bitVector;
         }
     }
 }
--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,3 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Collections;
+using System.Globalization;
+using System.Linq.Expressions;
+using Utilities;
+
+var inputReader = new InputReader("sampleInput.txt").AllLines;
+// var inputReader = new InputReader("input.txt").AllLines;
+
+var hexValues = new Hashtable
+{
+    { '0', "0000" },
+    { '1', "0001" },
+    { '2', "0010" },
+    { '3', "0011" },
+    { '4', "0100" },
+    { '5', "0101" },
+    { '6', "0110" },
+    { '7', "0111" },
+    { '8', "1000" },
+    { '9', "1001" },
+    { 'A', "1010" },
+    { 'B', "1011" },
+    { 'C', "1100" },
+    { 'D', "1101" },
+    { 'E', "1110" },
+    { 'F', "1111" }
+};
+
+ReadOnlySpan<char> decodedInput = string.Concat(inputReader[0].Select(s => hexValues[s]));
+
+var blah = BitVectorExtensions.GetSection(3);
+
+var position = 0;
+
+while (position < decodedInput.Length)
+{
+    var packetVersion = decodedInput[VersionRange()].ToBitVector()[blah];
+    var packetType = decodedInput[TypeRange()].ToBitVector()[blah];
+
+    switch (packetType)
+    {
+        case 4:
+            var numberBits = string.Empty;
+            
+            while (true)
+            {
+                var groupType = decodedInput[NumberGroupTypeRange()];
+                
+                numberBits += decodedInput[LiteralNumberGroupRange()].ToString();
+                
+                if (groupType[0] == '0')
+                {
+                    break;
+                }
+            }
+            
+            var number = numberBits.ToBitVector()[BitVectorExtensions.GetSection(numberBits.Length)];
+            
+            break;
+        
+        default:
+            break;
+    }
+}
+
+Range VersionRange() => position..(position += 3);
+Range TypeRange() => position..(position += 3);
+Range NumberGroupTypeRange() => position..(position += 1);
+Range LiteralNumberGroupRange() => position..(position += 4);
+
+void ReadPacket(int position)
+{
+    
+}
